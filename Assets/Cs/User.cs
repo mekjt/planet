9b44a16dc584aeebc2489b8153fcc0bfc39e1781
mekjt <gugjunghwa2@gmail.com>
2026-06-01 12:17:59 +0900

    User Code teat02

--- Assets/Cs/User.cs
+++ Assets/Cs/User.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class User : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+   public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            Debug.Log("좌측");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("우측");
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("점프!");
+        }
     }
 }
