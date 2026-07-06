dba3fdefc5c9b3e28e903c9bea0794f337af7017
mekjt <gugjunghwa2@gmail.com>
2026-07-06 12:15:47 +0900

    자원을 위한 데이터 추가
    기본 자원 셋을 위한 자원 코드 추가

---
+++ Assets/Cs/resources.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public int resourceshp = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
