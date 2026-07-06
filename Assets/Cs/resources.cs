9e2ec8eef5b3dbd1958b87d6124cfb4882d8ec09
mekjt <gugjunghwa2@gmail.com>
2026-07-06 13:19:13 +0900

    자원
    자원의 이름/자원의 채력등의 값을 저장할 변수 생성

--- Assets/Cs/resources.cs
+++ Assets/Cs/resources.cs
@@ -1,8 +1,11 @@
+using System.Globalization;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public int resourceshp = 10;
+    public int resourceshp;
+    public int resourcesdropcount;
+    public string resourcesname;
     void Start()
     {
         
